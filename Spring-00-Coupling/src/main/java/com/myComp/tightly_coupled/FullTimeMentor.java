2a5eb88733e376111a39a69b7206d97f60e9ddd9
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-29 13:03:48 +0300

    tightly coupled

---
+++ Spring-00-Coupling/src/main/java/com/myComp/tightly_coupled/FullTimeMentor.java
@@ -0,0 +1,9 @@
+package com.myComp.tightly_coupled;
+
+public class FullTimeMentor {
+
+    public void createAccount(){
+        System.out.println("Full Time Mentor Account is created.");
+    }
+
+}
