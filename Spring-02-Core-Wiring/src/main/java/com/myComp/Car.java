3f4f1561103f9e090c541296c982261b8b24e1d3
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-03 16:50:52 +0300

    wiring

---
+++ Spring-02-Core-Wiring/src/main/java/com/myComp/Car.java
@@ -0,0 +1,13 @@
+package com.myComp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Car {
+
+    private String make;
+}
---
+++ Spring-02-Core-Wiring/src/main/java/com/myComp/Person.java
@@ -0,0 +1,16 @@
+package com.myComp;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Person {
+
+    private String name;
+    private Car car;
+
+}
