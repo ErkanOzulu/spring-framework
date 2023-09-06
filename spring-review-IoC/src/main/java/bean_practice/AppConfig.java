5b20cf2a5287e476692c059ea1411e334f3c39bd
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-06 16:56:01 +0300

    Task1

--- spring-review-IoC/src/main/java/bean_practice/AppConfig.java
+++ spring-review-IoC/src/main/java/bean_practice/AppConfig.java
@@ -1,7 +1,9 @@
 package bean_practice;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
--- spring-review-IoC/src/main/java/bean_practice/MyApp.java
+++ spring-review-IoC/src/main/java/bean_practice/MyApp.java
@@ -8,12 +8,19 @@ public class MyApp {
 
 
         //how to create a spring container?
-        ApplicationContext container=new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext container=new AnnotationConfigApplicationContext(AppConfig.class, AnotherConfig.class);
 
         FullTimeEmployee fullTimeEmployee = container.getBean(FullTimeEmployee.class);
         fullTimeEmployee.createAccount();
+
         PartTimeEmployee partTimeEmployee = container.getBean(PartTimeEmployee.class);
         partTimeEmployee.createAccount();
+
+        String str1 = container.getBean("String 1",String.class);
+        System.out.println(str1);
+        String str2 = container.getBean("str2",String.class);
+        System.out.println(str2);
+
 
 
     }
