2231e8db7d5d014f2157890642cc591231c572fd
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-08 14:37:51 +0300

    lab-00-coupling

---
+++ Spring-00-Coupling/src/main/java/com/lab_00_coupling/loosely/BalanceManager.java
@@ -0,0 +1,10 @@
+package com.lab_00_coupling.loosely;
+
+public class BalanceManager {
+    public boolean checkout(
+            // add method parameters
+    ){
+        // implement checkout business
+        return false;
+    }
+}
