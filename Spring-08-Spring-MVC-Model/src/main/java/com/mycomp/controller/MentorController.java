dcef20988e3608833727bd8930d727c45c497363
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-09 19:35:03 +0300

    mentor task

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/mycomp/controller/MentorController.java
@@ -0,0 +1,34 @@
+package com.mycomp.controller;
+
+import com.mycomp.enums.Gender;
+import com.mycomp.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String mentor(Model model) {
+
+        List<Mentor> mentors = new ArrayList<>();
+
+        Mentor m1 = new Mentor("Cely", "Abc", 30, Gender.FEMALE);
+        Mentor m2 = new Mentor("Carl", "Yuan", 25, Gender.MALE);
+        mentors.addAll(Arrays.asList(m1, m2));
+
+        mentors.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
+        mentors.add(new Mentor("Tom", "Hanks", 65, Gender.MALE));
+        mentors.add(new Mentor("Ammy", "Bryan", 25, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentors);
+
+        return "mentor/mentor-list";
+    }
+
+}
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/mycomp/model/Mentor.java
@@ -0,0 +1,15 @@
+package com.mycomp.model;
+
+import com.mycomp.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Mentor {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
+
+}
