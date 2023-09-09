be6c66a0588dbb4c6f011307c714188317e69dfc
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-09 18:34:44 +0300

    Spring -MVC- module

--- Spring-08-Spring-MVC-Model/src/main/java/com/mycomp/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/mycomp/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.mycomp.controller;
 
+import org.apache.catalina.LifecycleState;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -11,6 +16,19 @@ public class StudentController {
     public String homePage(Model model) {
         model.addAttribute("name", "Mycomp");
         model.addAttribute("course", "MVC");
+
+        String subject="Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId(0-1000) and show it in your UI
+        int studentId= new Random().nextInt(1000);
+        model.addAttribute("id",studentId);
+        List<Integer>numbers=new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        model.addAttribute("numbers",numbers);
 
         return "student/welcome";
     }
