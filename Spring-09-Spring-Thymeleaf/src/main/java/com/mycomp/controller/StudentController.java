4cbcffe08de015afc10e241d0b6b464ae780f61a
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-16 22:37:10 +0300

    Spring Thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/mycomp/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/mycomp/controller/StudentController.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StudentController {
 
     @RequestMapping("/register")
-    public String register(Model model){
+    public String register(Model model) {
 
         model.addAttribute("students", DataGenerator.createStudent());
 
         return "student/register";
+    }
+
+    @RequestMapping("/welcome")
+    public String info() {
+        return "student/welcome";
     }
 }
