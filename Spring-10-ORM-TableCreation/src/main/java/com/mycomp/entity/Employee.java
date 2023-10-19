7194d4f0c2c9e1f14de1a5d339169f3d7e97f539
Erkan <ErkanOzulu@users.noreply.github.com>
2023-10-19 14:17:18 +0300

    some column annotation

--- Spring-10-ORM-TableCreation/src/main/java/com/mycomp/entity/Employee.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/mycomp/entity/Employee.java
@@ -1,11 +1,14 @@
 package com.mycomp.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Employee {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    private int id;
+    private int id; //primary key
     private String name;
 }
--- Spring-10-ORM-TableCreation/src/main/java/com/mycomp/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/mycomp/entity/Student.java
@@ -1,13 +1,32 @@
 package com.mycomp.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.mycomp.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")
 public class Student {
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //generate auto id
     @Id
     private Long id;
-    private String firstName;  //first_name
+    @Column(name = "studentFirstName")
+    private String firstName;
+    @Column(name = "studentLastName",length = 100)
     private String lastName;
     private String email;
+    @Transient //This column doesn't show in table
+    private String city;
+    @Column(columnDefinition = "Date")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "Time")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "Timestamp")
+    private LocalDate birthDateTime;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
 }
