d543f89fc95b7256d4979fcd6fdc694743b5cefe
Erkan <ErkanOzulu@users.noreply.github.com>
2023-11-22 11:01:09 +0300

    toString methods arranged according to FetchType.Lazy

--- Spring-13-ORM-CinemaLab/src/main/java/com/mycomp/entity/Genre.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/mycomp/entity/Genre.java
@@ -1,19 +1,30 @@
 package com.mycomp.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import net.bytebuddy.agent.builder.AgentBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Genre extends BaseEntity{
     private String name;
 
     @ManyToMany(mappedBy = "genreList")
     private List<Movie>movieList;
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
