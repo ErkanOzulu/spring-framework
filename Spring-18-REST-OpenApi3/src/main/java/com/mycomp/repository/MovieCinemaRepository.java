344889d0e783c8cbb8d30b9bbd68e48e91fe60c0
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-05 09:13:31 +0300

    OpenApi3

---
+++ Spring-18-REST-OpenApi3/src/main/java/com/mycomp/repository/MovieCinemaRepository.java
@@ -0,0 +1,9 @@
+package com.mycomp.repository;
+
+import com.mycomp.entity.MovieCinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
+}
---
+++ Spring-18-REST-OpenApi3/src/main/java/com/mycomp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mycomp.repository;
+
+import com.mycomp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
